688d8ee7e98cec85b3522b1a022cf88add9b4c46
Levi Klein Marifosque <levikleinm@gmail.com>
2021-04-18 16:01:44 +0800

    change lockfile to /home/pi

--- irrp.py
+++ irrp.py
@@ -64,7 +64,7 @@ import pigpio # http://abyz.co.uk/rpi/pigpio/python.html
 
 import fcntl
 
-LOCKFILE = '~/.irrp.lock'
+LOCKFILE = '/home/pi/.irrp.lock'
 
 f = open(LOCKFILE, "w")
 print("acquiring lock!")
